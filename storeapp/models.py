3ca485d244d0af09a3008e79dc56c0388a0c78ca
Camilla-aa <aletiacamilla7@gmail.com>
2026-02-23 20:32:50 +0300

    New Commit

--- storeapp/models.py
+++ storeapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    productname = models.CharField(max_length=50)
+    quantity = models.CharField(max_length=50)
+    price = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.productname
+
+   
